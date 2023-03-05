aba8b6d15d2920543c685e3f625e626261587133
Yash <ys77357@gmail.com>
2023-03-06 00:36:33 +0530

    Learnt about routing

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,8 +1,11 @@
 const fs = require("fs");
-
+const http = require("http");
+const url = require("url");
 // const hello = "Hello World!";
 // console.log(hello);
 
+////////////////////
+// FILES
 //synchronous way
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 // console.log(textIn);
@@ -11,15 +14,46 @@ const fs = require("fs");
 // console.log(`File Written!`);
 
 //asynchronous way
-fs.readFile("./txt/start.txt", "utf-8", (error, data1) => {
-  if (error) return console.error(`Error!`);
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (error, data2) => {
-    fs.readFile("./txt/append.txt", "utf-8", (error, data3) => {
-      console.log(data3);
-      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-        console.log(`Files have been written!`);
-      });
+// fs.readFile("./txt/start.txt", "utf-8", (error, data1) => {
+//   if (error) return console.error(`Error!`);
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (error, data2) => {
+//     fs.readFile("./txt/append.txt", "utf-8", (error, data3) => {
+//       console.log(data3);
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log(`Files have been written!`);
+//       });
+//     });
+//   });
+// });
+// console.log(`Will read file!`);
+
+////////////////////
+// SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  // console.log(req);
+  console.log(req.url);
+  const pathName = req.url;
+  if (pathName === "/" || pathName === "/overview") {
+    res.end(`This is the overview`);
+  } else if (pathName === "/product") {
+    res.end(`This is the product`);
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello world",
     });
-  });
+    res.end(`<h1>Page Not Found!!</h1>`);
+  }
+  // res.end(`Hello from the server!!`);
 });
-console.log(`Will read file!`);
+
+server.listen(9000, "127.0.0.1", () => {
+  console.log(`Listening to request on port 9000`);
+});
