928a820467cf2982336b88a690235a2ed0a5cc2b
Yash <ys77357@gmail.com>
2023-04-30 12:00:15 +0530

    Learnt about REST architecture and express basic

--- 4-natours/after-section-06/app.js
+++ 4-natours/after-section-06/app.js
@@ -29,3 +29,18 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 module.exports = app;
+
+app.get('/',(req,res)=>{{
+  // res.status(200).send('Hello from server side!!');
+  res.status(200).json({message:'Hello from server side!!',app:'Natours'});
+}})
+ 
+app.post('/',(req,res)=>{
+  res.send('You can post to this endpoint...')
+})
+
+const port=3000;
+app.listen(port,()=>{
+  console.log(`app running on port ${port}...`);
+});
+
